b18b8c4c4b4efcaba7f67975b9a867872527f52c
lktrgl <32882989+lktrgl@users.noreply.github.com>
2021-03-14 02:11:33 +0200

    added logical level support for Async Io and/or Event Io powered devices

--- include/ioapi/device.h
+++ include/ioapi/device.h
@@ -26,12 +26,34 @@ typedef int8_t device_id_t;
 
 /*---------------------------------------------------------------------------*/
 
+enum device_io_operation_enum
+{
+  DEVICE_OPERATION_READ,
+  DEVICE_OPERATION_WRITE,
+  DEVICE_OPERATION_IOCTL,
+};
+
+typedef struct device_io_result_tag
+{
+  uint8_t status;
+  uint32_t data_len;
+  void* data_src;
+  void* data_dest;
+} device_io_result_t;
+
+/*---------------------------------------------------------------------------*/
+
 typedef int8_t ( *device_init_t ) ();
 typedef int8_t ( *device_open_t ) ( const char* device_name );
 typedef int16_t ( *device_read_t ) ( void* dest, uint16_t len );
 typedef int16_t ( *device_write_t ) ( const void* src, uint16_t len );
 typedef int16_t ( *device_ioctl_t ) ( uint16_t operation, void* ptr );
 typedef void ( *device_close_t ) ( device_id_t id );
+
+/*---------------------------------------------------------------------------*/
+
+typedef void ( *device_callback_io_complete_t ) ( uint8_t io_operation, device_io_result_t* ptr );
+typedef void ( *device_callback_event_t ) ( uint8_t io_event );
 
 /*---------------------------------------------------------------------------*/
 
@@ -44,6 +66,13 @@ typedef struct device_descriptor_tag
   device_write_t write;
   device_ioctl_t ioctl;
   device_close_t close;
+
+#ifdef IOAPI_CALLBACK_IO_COMPLETE
+  device_callback_io_complete_t io_complete;
+#endif
+#ifdef IOAPI_CALLBACK_IO_EVENT
+  device_callback_event_t event;
+#endif
 } device_descriptor_t;
 
 /*---------------------------------------------------------------------------*/
